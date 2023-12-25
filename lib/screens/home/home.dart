af06e2d0877e5fd4381fab987c13be84eda3ee1d
Retouchable <109464156+Retouchable@users.noreply.github.com>
2023-12-25 14:28:00 +0700

    Home update
    small update

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,7 +8,6 @@ class Home extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         useMaterial3: true,
-
         // Define the default brightness and colors.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.purple,
@@ -30,9 +29,80 @@ class Home extends StatelessWidget {
             title: const Text('NuwunSewu'),
             backgroundColor: Colors.purple[100],
           ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Icon(Icons.directions_car),
+              ListView(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  // ini seharusnya return a widget
+                  Container(
+                    height: 350,
+                    width: 300,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.elliptical(20, 20)),
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    child: Container(
+                      margin: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // gambar post
+                          Container(
+                            padding: const EdgeInsets.all(90),
+                            height: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.green,
+                            ),
+                          ),
+                          // tombol like, share, etc.
+                          Row(
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.heart_broken)),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.chat_bubble_rounded)),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.share)),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              // gambar profile, bisa langusng CircleAvatar(Image)
+                              const CircleAvatar(),
+                              // konteks
+                              Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: const Column(
+                                  children: [
+                                    Text("Account"),
+                                    Text(
+                                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra sit amet aliquam id diam. Pulvinar neque laoreet suspendisse interdum. Nulla porttitor massa id neque aliquam vestibulum morbi. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt.",
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          // masih bingung biar langsung taruh paling bawah
+                          const Row(
+                            children: [Text("Hours")],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Icon(Icons.directions_transit),
               Icon(Icons.directions_bike),
             ],
