b08bc4a9d4610fe7aab1841ef9343a26ec5aad6d
Retouchable <109464156+Retouchable@users.noreply.github.com>
2023-12-27 16:29:40 +0700

    Floatting Button for Post Page
    add float button post, remove post in bottombar

--- lib/screens/home/upload.dart
+++ lib/screens/home/upload.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nuwunsewu/screens/home/home.dart';
+import 'package:nuwunsewu/screens/home/navigation.dart';
 
 class Upload extends StatelessWidget {
   const Upload({super.key});
@@ -19,7 +21,11 @@ class Upload extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
-          title: Text('Upload Postingan'),
+          leading: IconButton(onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Navigation()));
+          }, icon: const Icon(Icons.arrow_back_ios_rounded)),
+          title: const Text('Upload Postingan'),
           backgroundColor: Colors.purple[100],
           elevation: 0.0,
         ),
